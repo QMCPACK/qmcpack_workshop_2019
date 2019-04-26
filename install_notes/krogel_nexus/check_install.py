3b7b4dd2c4917d8fab223ca8a3e33c17d3c7fb11
Jaron Krogel <krogeljt@ornl.gov>
2019-04-26 12:17:51 -0400

    add nexus install notes

---
+++ install_notes/krogel_nexus/check_install.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+import numpy
+import scipy
+import h5py
+import matplotlib
+import pydot
+import spglib
+import seekpath
+
+from nexus import *
+
+print 'nexus install should work!'
+
