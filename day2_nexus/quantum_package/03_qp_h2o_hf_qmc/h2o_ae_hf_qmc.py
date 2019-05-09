dca03a7334c2039f062a5a8bbe3ce942580ee0d3
Jaron Krogel <krogeljt@ornl.gov>
2019-05-09 09:57:26 -0400

    add block statements to qp example 3

--- day2_nexus/quantum_package/03_qp_h2o_hf_qmc/h2o_ae_hf_qmc.py
+++ day2_nexus/quantum_package/03_qp_h2o_hf_qmc/h2o_ae_hf_qmc.py
@@ -60,6 +60,7 @@ cc = generate_cusp_correction(
 
 # optimize 2-body Jastrow
 optJ2 = generate_qmcpack(
+    block           = True,
     identifier      = 'opt',
     path            = 'H2O/optJ2',
     job             = qmc_job,
@@ -78,6 +79,7 @@ optJ2 = generate_qmcpack(
 
 # optimize 3-body Jastrow
 optJ3 = generate_qmcpack(
+    block           = True,
     identifier      = 'opt',
     path            = 'H2O/optJ3',
     job             = qmc_job,
@@ -96,12 +98,15 @@ optJ3 = generate_qmcpack(
 
 # run VMC with QMCPACK
 qmc = generate_qmcpack(
+    block        = True,
     identifier   = 'vmc',
     path         = 'H2O/vmc',
     job          = qmc_job,
     system       = system,
     jastrows     = [],
     qmc          = 'vmc',    # use vmc defaults
+    blocks       = 800,
+    steps        = 100,
     dependencies = [(c4q,'orbitals'),
                     (cc,'cuspcorr'),
                     (optJ3,'jastrow')],
@@ -109,6 +114,7 @@ qmc = generate_qmcpack(
 
 # run DMC with QMCPACK
 qmc = generate_qmcpack(
+    block        = True,
     identifier   = 'dmc',
     path         = 'H2O/dmc',
     job          = qmc_job,
