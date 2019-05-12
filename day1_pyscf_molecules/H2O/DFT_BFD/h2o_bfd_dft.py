a5102fe7da497e8b06c1e6117a66ecfb50e73b02
anbenali <abenali.sci@gmail.com>
2019-05-12 03:12:36 -0500

    pyscf Molecular tests. Includes All elec DFT and HF, and ECP-BFD, ECP-NCSU DFT

---
+++ day1_pyscf_molecules/H2O/DFT_BFD/h2o_bfd_dft.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from pyscf import gto
+from pyscf import scf, dft, df
+
+mol = gto.Mole()
+mol.verbose = 5
+mol.atom =''' 
+  H             0.00000000       0.75720000      -0.46920000
+  H             0.00000000      -0.75720000      -0.46920000
+  O             0.00000000       0.00000000       0.11730000
+ '''
+mol.unit='A'
+mol.basis = 'bfd_vtz'
+mol.ecp = 'bfd'
+mol.spin=0 #Value of S where the spin multiplicity is 2S+1
+mol.build()
+
+
+
+#Hartree Fock
+#mf = scf.ROHF(mol)
+
+#DFT
+mf = dft.ROKS(mol)
+mf.xc ='b3lyp' 
+
+e_scf=mf.kernel()
+
+#Section for QMCPACK
+title="H2O_DFT_BFD"
+from PyscfToQmcpack import savetoqmcpack
+savetoqmcpack(mol,mf,title=title)
+
